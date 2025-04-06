53d509511cb2c135c1d8f2dce803a22735001724
Chris Walker <chris@awsm.tech>
2025-04-06 13:06:42 +0100

    WIP adding a index pager

--- crates/engine/src/index_pager.rs
+++ crates/engine/src/index_pager.rs
@@ -4,7 +4,7 @@ use std::{
 };
 
 use crate::{
-    page::PageId,
+    page::{PageDecoder, PageId},
     page_cache::{FilePageId, PageCache},
 };
 
@@ -23,31 +23,37 @@ impl IndexPager {
     }
 }
 
-pub struct IndexPagerIterator<'a> {
-    root_page: PageId,
+pub struct IndexPagerIterator {
     file_id: u16,
     current_page: PageId,
-    page_cache: RefMut<'a, PageCache>,
+    current_page_slot: u16,
+    page_cache: Rc<RefCell<PageCache>>,
 }
 
-impl<'a> IndexPagerIterator<'a> {
+impl IndexPagerIterator {
     pub fn new(index_root_file_page_id: FilePageId, page_cache: Rc<RefCell<PageCache>>) -> Self {
         IndexPagerIterator {
-            current_page: index_root_file_page_id.page_index,
-            root_page: index_root_file_page_id.page_index,
             file_id: index_root_file_page_id.db_id,
-            page_cache: page_cache.borrow_mut(),
+            current_page: index_root_file_page_id.page_index,
+            current_page_slot: 0,
+            page_cache,
         }
     }
 }
 
-impl<'a> Iterator for IndexPagerIterator<'a> {
+impl Iterator for IndexPagerIterator {
     type Item = Vec<u8>;
 
     fn next(&mut self) -> Option<Self::Item> {
-        let _curr_page = self
-            .page_cache
-            .get_page(&FilePageId::new(self.file_id, self.current_page));
+        let page_cache = self.page_cache.borrow_mut();
+
+        let page_bytes = page_cache.get_page(&FilePageId::new(self.file_id, self.current_page))?;
+        let page = PageDecoder::from_bytes(&page_bytes);
+
+        // TODO: what do we read?
+        let slot = page.try_read(self.current_page_slot);
+
+        self.current_page_slot += 1;
 
         todo!()
     }
