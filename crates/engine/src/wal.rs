0606a1c9851f4865f6c9f64aa9800d4dbf681ecc
WhatFor <chris@chriswalker.soy>
2026-04-21 21:51:47 +0100

    Start working on WAL; First draft WalLog struct

--- crates/engine/src/wal.rs
+++ crates/engine/src/wal.rs
@@ -1,6 +1,6 @@
 use anyhow::Result;
 use deku::prelude::DekuSize;
-use deku::{DekuRead, DekuWrite};
+use deku::{ctx::Endian, DekuRead, DekuWrite};
 use thiserror::Error;
 
 /// The max, current version number for the Log Header record
@@ -47,4 +47,74 @@ impl WalHeader {
             Err(WalError::MagicStringInvalid.into())
         }
     }
+}
+
+#[derive(DekuRead, DekuWrite, Debug, PartialEq)]
+#[deku(
+    id_type = "u8",
+    endian = "endian",
+    ctx = "endian: deku::ctx::Endian",
+    ctx_default = "Endian::Big"
+)]
+pub enum LogType {
+    #[deku(id = 0)]
+    Insert,
+    #[deku(id = 1)]
+    Update,
+    #[deku(id = 2)]
+    Delete,
+    #[deku(id = 3)]
+    Checkpoint,
+    #[deku(id = 4)]
+    Commit,
+}
+
+#[derive(DekuRead, DekuWrite, Debug, PartialEq)]
+#[deku(endian = "big")]
+pub struct WalLog {
+    #[deku(bytes = 4)]
+    pub lsn: u32,
+    #[deku(bytes = 4)]
+    pub prev_lsn: u32,
+    #[deku(bytes = 8)]
+    pub transaction_id: u64,
+    #[deku]
+    pub log_type: LogType,
+    #[deku(bytes = 2)]
+    pub checksum: u16, // Big endian
+    #[deku(bytes = 1)]
+    pub payload_len: u16,
+    #[deku(bytes = 65_536, count = "payload_len")] // TODO: is 65kb enough?
+    pub payload: Vec<u8>,
+}
+
+impl WalLog {
+    pub fn new(
+        lsn: u32,
+        prev_lsn: Option<u32>,
+        txn_id: Option<u64>,
+        log_type: LogType,
+        payload: Vec<u8>,
+    ) -> Self {
+        let crc = check(&payload);
+        let checksum = u16::from_be_bytes(crc);
+
+        WalLog {
+            lsn,
+            prev_lsn: prev_lsn.unwrap_or(0),
+            transaction_id: txn_id.unwrap_or(0),
+            log_type: log_type,
+            checksum: checksum,
+            payload_len: payload
+                .len()
+                .try_into()
+                .expect("Max payload length of 65kb exceeded"),
+            payload: payload,
+        }
+    }
+}
+
+fn check(bytes: &[u8]) -> [u8; 2] {
+    let crc = crc::Crc::<u16>::new(&crc::CRC_16_IBM_SDLC);
+    crc.checksum(bytes).to_be_bytes()
 }
